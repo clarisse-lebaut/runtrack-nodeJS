bce24ad5269898d0285d6074f3ff890519aadbe1
clarisse.lebaut <clarisse.le-baut@laplateforme.io>
2024-05-14 10:20:14 +0200

    export / import du serveur dans un autre fichier

--- Jour02/index.js
+++ Jour02/index.js
@@ -1,1 +1,10 @@
 // point d'entrée de l'API
+
+//on importe le serveur dans ce fichier
+const server = require('./server');
+
+const port = 8888;
+
+server.listen(port, () => {
+    console.log (`Lien pour le serveur : http://localhost:${port}/`)
+})
