d09bf2ee43d2320c321ab1a01aebf3ad5623d762
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-05-17 11:42:14 +0200

    modification de la page index (ajout une image)

--- Jour05/runtrack_express/projet_web/index.js
+++ Jour05/runtrack_express/projet_web/index.js
@@ -2,10 +2,15 @@
 
 const express = require("express");
 const app = express(); // création d'une instance d'Express
+const path = require("path"); // permet de récupérer les chemins des fichiers qui seront envoyé comme réponse
+
+// serveur static qui permet de servir des fichier static
+// cette méthode permet de servir des fichier statics
+app.use(express.static("publics"));
 
 //  première page
 app.get("/", (req, res) => {
-  res.send("page d'acceuil du site");
+  res.sendFile(path.join(__dirname, "views", "index.html")); // important de mettre un double undescrore --> permet de faire correctement la syntaxe
 });
 
 // deuxième page
@@ -13,7 +18,14 @@ app.get("/about", (req, res) => {
   res.send("page a propos qui présente le projet");
 });
 
+// troisième page
+app.get("/error404", (req, res) => {
+  res.send("page 404");
+});
+
+// permet de lancer le serveur afin de rendre accessible les éléments
 app.listen(80, () => {
   console.log("Server listening on port 3000");
   console.log(`http://localhost:80`);
+  console.log("n'oublie de faire un CTRL+C pour fermer le serveur");
 });
